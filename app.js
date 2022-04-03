a3183633b43d2567350c01d1d52490be01a0d3ad
Sibasis-Malla <120cs0179@nitrkl.ac.in>
2022-04-03 10:57:18 +0530

    apiSetup

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
 const express = require('express');
 const request = require('request');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  request(
-    { url: 'https://cors-anywhere.herokuapp.com/https://livepeer.com/api/stream' },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: error.message });
+app.get('/api/stream/:APIkey', async(req, res) => {
+    const { APIkey } = req.params;
+    console.log(APIkey)
+   await axios
+    .post(
+      "https://livepeer.com/api/stream",
+      {
+        name: "test_stream",
+        profiles: [
+          {
+            name: "720p",
+            bitrate: 2000000,
+            fps: 30,
+            width: 1280,
+            height: 720,
+          },
+          {
+            name: "480p",
+            bitrate: 1000000,
+            fps: 30,
+            width: 854,
+            height: 480,
+          },
+          {
+            name: "360p",
+            bitrate: 500000,
+            fps: 30,
+            width: 640,
+            height: 360,
+          },
+        ],
+      },
+      {
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${APIkey}`,
+        },
       }
-
-      res.json(JSON.parse(body));
-    }
-  )
+    ).then((response)=>{
+        console.log("worked");
+        console.log(response.data.streamKey)
+        res.json(response.data);
+    }).catch( error => {
+        console.error(`Could not get products: ${error}`);
+    });
 });
 
 const PORT = process.env.PORT || 2000;
