eddf9d0d19ba1a7f91571ebf584ee92cd4e189a9
Sibasis-Malla <120cs0179@nitrkl.ac.in>
2022-07-05 01:11:09 +0530

    changes

--- app.js
+++ app.js
@@ -8,52 +8,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/stream/:APIkey', async(req, res) => {
-    const { APIkey } = req.params;
-    console.log(APIkey)
+app.get('/api/data/:tweetId', async(req, res) => {
+    const { tweetId } = req.params;
+    console.log(tweetId)
    await axios
-    .post(
-      "https://livepeer.com/api/stream",
-      {
-        name: "test_stream",
-        profiles: [
-          {
-            name: "720p",
-            bitrate: 2000000,
-            fps: 30,
-            width: 1280,
-            height: 720,
-          },
-          {
-            name: "480p",
-            bitrate: 1000000,
-            fps: 30,
-            width: 854,
-            height: 480,
-          },
-          {
-            name: "360p",
-            bitrate: 500000,
-            fps: 30,
-            width: 640,
-            height: 360,
-          },
-        ],
-      },
-      {
-        headers: {
-          "content-type": "application/json",
-          authorization: `Bearer ${APIkey}`,
-        },
-      }
-    ).then((response)=>{
-        console.log("worked");
-        console.log(response.data.streamKey)
-        res.json(response.data);
+    .get(
+      `https://publish.twitter.com/oembed?url=https://twitter.com/Interior/status/${tweetId}&theme=dark&omit_script=1`
+ ).then((response)=>{
+        // console.log("worked");
+        // console.log(response.data.streamKey)
+        //console.log(response)
+       res.json(response.data);
     }).catch( error => {
         console.error(`Could not get products: ${error}`);
+        res.json(error)
     });
 });
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+//app.listen(8080);
